39fe7ec5c2e109107e61f8ff3e1ca1dfcd63099f
Francesca Tripodi <fbt8pa@virginia.edu>
2013-12-04 14:39:44 -0500

    made address

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+<?php
+	$address = "609 St. Charles Ave";
+	
+?>
+
 <html>
 <head>
-	<title>Example 1</title>
+	<title>Example 1</title> 
+	<style type="text/css"> 
+	h1 {font-size: 1.8em;}
+
+
+
+	</style>
 </head>
 <body>
 <h1>My Website</h1>
 <p><?php echo 'Is dinosaur free'; ?></p>
+
+
+<p><?php echo $address; ?></p>
+
 </body>
 </html>
